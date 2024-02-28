7dbb794c65bcbf4fa15d7bb0529a0b5b74411b38
andreaalarconvaldes <andreaalarconvaldes@gmail.com>
2024-02-28 20:19:11 +0100

    Create Contact page

---
+++ src/pages/Contact/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+import { ContactForm } from "./ContactForm";
+
+export function Contact() {
+  return (
+    <>
+    <h3>Get in touch</h3>
+   <ContactForm/>
+   </>
+  );
+}
