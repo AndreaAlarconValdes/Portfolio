3e4034abe65e46b4bbef3ad0473d141917fd212c
andreaalarconvaldes <andreaalarconvaldes@gmail.com>
2024-02-29 21:16:56 +0100

    Fix contact page styles

--- src/pages/Contact/ContactForm.jsx
+++ src/pages/Contact/ContactForm.jsx
@@ -26,30 +26,24 @@ export function ContactForm() {
   };
 
   return (
-    <section className="contact-container">
-      <div className="message-form">
-        <h3>Send a message</h3>
-        <form className="contact-form" ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input required type="text" name="from_name" />
-          <label>Email</label>
-          <input required type="email" name="from_email" />
-          <label>Message</label>
-          <textarea
-            required
-            name="message"
-            placeholder="Hi Andrea, we would like to get in touch with you..."
-          />
-          <button type="submit">Send Email</button>
-        </form>
-      </div>
-      <div className="contact-info">
-        <h3>Contacts</h3>
-        <span>Email</span>
-        <p>andreaalarconvaldes@gmail.com</p>
-        <span>Phone number</span>
-        <p>+34 644 403 445</p>
-      </div>
+    <section>
+      <h1>
+        Fill the form 
+        <br/>to <strong className="main-title">get in touch</strong>{" "}
+      </h1>
+      <form className="contact-form" ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input required type="text" name="from_name" />
+        <label>E-mail</label>
+        <input required type="email" name="from_email" />
+        <label>Message</label>
+        <textarea
+          required
+          name="message"
+          placeholder="Hi Andrea, we would like to get in touch with you..."
+        />
+        <button type="submit">Send message</button>
+      </form>
     </section>
   );
 }
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,11 +1,16 @@
 import "./index.css";
 import { ContactForm } from "./ContactForm";
+import { ContactInformation } from "./ContactInformation";
+import { Blob, WaveFooter } from "../../components/Blobs";
 
 export function Contact() {
   return (
     <>
-    <h3>Get in touch</h3>
-   <ContactForm/>
-   </>
+    <main className="contact-container">
+      <ContactForm />
+      <ContactInformation />
+    </main>
+      <WaveFooter color="#5f5fdb"/>
+      </>
   );
 }
